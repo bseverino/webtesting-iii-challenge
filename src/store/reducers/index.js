62464242b9546d123fc8203fd5d90a031b42f609
B Severino <biancasev@gmail.com>
2019-12-16 16:55:10 -0500

    Added redux structure

---
+++ src/store/reducers/index.js
@@ -0,0 +1,21 @@
+import { TOGGLE_LOCKED, TOGGLE_CLOSED } from '../actions';
+
+const initialState = {
+    locked: false,
+    closed: false
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case TOGGLE_LOCKED:
+            return {
+                ...state,
+                locked: !state.locked
+            };
+        case TOGGLE_CLOSED:
+            return {
+                ...state,
+                closed: !state.closed
+            };
+    };
+};
