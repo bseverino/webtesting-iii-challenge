5f975164ccfad6838ce32ea31d588a8c59489e00
B Severino <biancasev@gmail.com>
2019-12-16 16:43:54 -0500

    Tested for correct display colors

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { toHaveClass } from '@testing-library/jest-dom';
+expect.extend({ toHaveClass })
 
 import Display from './Display';
 
@@ -15,4 +17,18 @@ test('Displays if the gate is closed or open', () => {
     getByText(/closed/i);
     rerender(<Display closed={false} />);
     getByText(/open/i);
+});
+
+test('If locked or closed, the display is red', () => {
+    const { getByText, rerender } = render(<Display locked />);
+    expect(getByText('Locked')).toHaveClass('red-led');
+    rerender(<Display closed />);
+    expect(getByText(/closed/i)).toHaveClass('red-led');
+});
+
+test('If unlocked or open, the display is green', () => {
+    const { getByText, rerender } = render(<Display locked={false} />);
+    expect(getByText(/unlocked/i)).toHaveClass('green-led');
+    rerender(<Display closed={false} />);
+    expect(getByText(/open/i)).toHaveClass('green-led');
 });
